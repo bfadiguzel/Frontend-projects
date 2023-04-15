91ede58e5b94809beb60dec6222ec091ab0f8ab9
Bilal Fatih Adıgüzel <bilalgsda@gmail.com>
2023-04-16 02:43:24 +0300

    dadjoke(API)

---
+++ dadjoke(API)/script.js
@@ -0,0 +1,16 @@
+const jokeElement = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+jokeBtn.addEventListener('click', () => generateJoke());
+generateJoke();
+
+/* Async Await*/
+async function generateJoke(){
+    const config ={
+        headers:{
+            'Accept':'application/json'
+        }
+    };
+   const res = await fetch('https://icanhazdadjoke.com',config)
+   const data = await res.json();
+   jokeElement.innerHTML = data.joke;
+}
