720521c42db324ec56b9e40e77a902375dba9ca9
Bilal Fatih Adıgüzel <bilalgsda@gmail.com>
2023-04-17 00:29:15 +0300

    Faq Collapse

---
+++ Faq Collapse/script.js
@@ -0,0 +1,4 @@
+const faqButtons = document.querySelectorAll(".faq-toggle");
+faqButtons.forEach(button =>{
+    button.addEventListener('click',()=> button.parentNode.classList.toggle("active"));
+});
