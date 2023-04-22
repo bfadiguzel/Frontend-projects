617baf7d73295d0d651da0aabec973b22320c733
Bilal Fatih Adıgüzel <bilalgsda@gmail.com>
2023-04-22 03:23:12 +0300

    Movie App

---
+++ Movie App/script.js
@@ -0,0 +1,60 @@
+const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5ebda5f7997169dfbf6b2b961ab47bae&page=${getRandomPage()}`
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280"
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=5ebda5f7997169dfbf6b2b961ab47bae&query="'
+const form = document.getElementById("form")
+const main = document.getElementById("main")
+const search = document.getElementById("search")
+getMovies(API_URL)
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const searchTem = search.value;
+    if (searchTem && searchTem != '') {
+        getMovies(SEARCH_API + searchTem)
+        search.value = ''
+    }
+    else {
+        window.location.reload()
+    }
+
+})
+async function getMovies(url) {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    showMovies(data.results)
+}
+
+function showMovies(movies) {
+    main.innerHTML = ''
+
+    movies.forEach(movie => {
+        const { title, poster_path, vote_average, overview } = movie
+
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+        movieEl.innerHTML = `
+        
+        <img src="${IMG_PATH + poster_path}">
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+            <h3>Overview</h3>
+            ${overview}
+        </div>
+   
+        `
+        main.appendChild(movieEl)
+    });
+}
+
+function getClassByRate(vote){
+    if(vote>=8) return "green"
+    if(vote >=5) return "orange"
+    return "red"
+}
+function getRandomPage(){
+    return Math.floor(Math.random() * 500)
+}
