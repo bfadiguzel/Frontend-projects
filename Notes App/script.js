9031ecf1f1765e510b44aa9bb697b5c5127ba129
Bilal Fatih Adıgüzel <bilalgsda@gmail.com>
2023-05-09 01:54:58 +0300

    Notes App

---
+++ Notes App/script.js
@@ -0,0 +1,65 @@
+const addBtn = document.getElementById('add')
+const notes = JSON.parse(localStorage.getItem('notes'))
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (notes) {
+        notes.forEach(note => addNewNote(note))
+    }
+})
+
+const addNewNote = (text = null) => {
+    const note = document.createElement('div')
+    note.classList.add('note')
+    note.innerHTML = `
+    <div class="tools">
+            <button class="edit"><i class="fas fa-edit"></i></button>
+            <button class="delete"><i class="fas fa-trash-alt"></i></button>
+    </div>
+
+    <div class="main ${text ? "" : "hidden"}"></div>
+    <textarea class="${text ? "hidden" : ""}"></textarea>
+    `
+
+    const delteBtn = note.querySelector('.delete')
+    const editBtn = note.querySelector('.edit')
+    const main = note.querySelector('.main')
+    const textArea = note.querySelector('textarea')
+
+    textArea.value = text
+    if(text)
+        main.innerHTML = marked(text)
+
+    delteBtn.addEventListener('click', () => {
+        note.remove()
+        updateLS()
+    })
+
+    editBtn.addEventListener('click', () => {
+        console.log('clicked')
+        main.classList.toggle('hidden')
+        textArea.classList.toggle('hidden')
+    })
+
+    textArea.addEventListener('input', (e) => {
+        const { value } = e.target
+
+        main.innerHTML = marked(value)
+
+        updateLS()
+    })
+
+    document.body.appendChild(note)
+}
+
+const updateLS = () => {
+    const notesText = document.querySelectorAll('textarea')
+
+    const notes = []
+
+    notesText.forEach(note => notes.push(note.value))
+
+    localStorage.setItem('notes', JSON.stringify(notes))
+
+}
+
+addBtn.addEventListener('click', () => addNewNote())
